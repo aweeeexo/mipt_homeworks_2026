064c276aa6c0d3312f47a964e63555e97478abda
Волченко Александр Максимович <qolchenkoalex@gmail.com>
2026-05-14 23:51:55 +0300

    Fix comparison operators in eviction logic

--- part4_oop/hw45.py
+++ part4_oop/hw45.py
@@ -38,7 +38,7 @@ class FIFOPolicy(Policy[K]):
             self._order.append(key)
 
     def get_key_to_evict(self) -> K | None:
-        if len(self._order) >= self.capacity:
+        if len(self._order) > self.capacity:
             return self._order[0]
         return None
 
@@ -65,7 +65,7 @@ class LRUPolicy(Policy[K]):
         self._order.append(key)
 
     def get_key_to_evict(self) -> K | None:
-        if len(self._order) >= self.capacity:
+        if len(self._order) > self.capacity:
             return self._order[0]
         return None
 
@@ -98,7 +98,7 @@ class LFUPolicy(Policy[K]):
             self._order.append(key)
 
     def get_key_to_evict(self) -> K | None:
-        if len(self._key_counter) < self.capacity:
+        if len(self._key_counter) <= self.capacity:
             return None
 
         min_keys = self._get_keys_with_min_freq()
